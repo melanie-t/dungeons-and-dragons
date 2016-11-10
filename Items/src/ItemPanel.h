ad6db4fecbea10221e6d11baa625566326ebe387
Melanie Taing <melanie_taing@hotmail.com>
2016-11-10 06:46:53 -0500

    Merged EquipPanel into ItemPanel. Items move while equipping. To create an item panel, include .h and call: ItemPanel::createInventories();

---
+++ Items/src/ItemPanel.h
@@ -0,0 +1,13 @@
+/** @file ItemPanel.h
+*	@brief Header file for ItemPanel class
+*   To create the item panel, include in main and call: 
+*   ItemPanel::createInventories();
+*/
+
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class ItemPanel {
+public:
+	static void createInventories();
+};
