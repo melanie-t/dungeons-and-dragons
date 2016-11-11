f1e2482407cd77e57f16a9e9ad5aaf2c954460b3
Melanie Taing <melanie_taing@hotmail.com>
2016-11-10 21:38:01 -0500

    Changed main menu into switch statements

--- Items/src/Main.cpp
+++ Items/src/Main.cpp
@@ -23,12 +23,21 @@ void consoleInput()
 	while (run)
 	{
 		cout << "What would you like to do? (enter number)" << endl;
-		cout << "1. Map Editor" << endl;
-		cout << "2. exit program" << endl;
-		string cmd = "";
+		cout << "1. Start game" << endl;
+		cout << "2. Create/Edit Map" << endl;
+		cout << "3. Create/Edit Character" << endl;
+		cout << "4. Create/Edit Item" << endl;
+		cout << "5. Exit program" << endl;
+		int cmd;
 		cin >> cmd;
 
-		if (cmd == "1") //edit map
+		switch (cmd)
+		{
+		case 1: // Start game
+		{
+			//Move stuff
+		} // Start Game FIN
+		case 2: // Create/Edit map 
 		{
 			bool mapedit = true;
 			while (mapedit)
@@ -36,120 +45,129 @@ void consoleInput()
 				cout << "What would you map editing function would you like to do? (enter number)" << endl;
 				cout << "1. Create Map" << endl;
 				cout << "2. Edit Map" << endl;
-				cout << "3. exit map editor" << endl;
+				cout << "3. Exit Map Editor" << endl;
 				string map_cmd;
-
 				std::cin >> map_cmd;
-
 				Map* map;
-				if (map_cmd == "1") // create map
-				{
-					int width, length;
-					cout << "What length should the map be?" << endl;
-					cin >> length;
-					cout << "What width should the map be?" << endl;
-					cin >> width;
-					//map = new Map();
-
-					//find a better way of doing this.
-					int id = 1;
-					CMarkup xml;
-					char di[20];
-					sprintf_s(di, 20, "maps/%d.xml", id);
-					while (xml.Load(string(di)))
+					if (map_cmd == "1") // create map
 					{
-						id++;
+						int width, length;
+						cout << "What length should the map be?" << endl;
+						cin >> length;
+						cout << "What width should the map be?" << endl;
+						cin >> width;
+						//map = new Map();
+
+						//find a better way of doing this.
+						int id = 1;
+						CMarkup xml;
+						char di[20];
 						sprintf_s(di, 20, "maps/%d.xml", id);
+						while (xml.Load(string(di)))
+						{
+							id++;
+							sprintf_s(di, 20, "maps/%d.xml", id);
+						}
+						map = new Map(id, length, width);
+						cout << "Created new map with ID: " << id << endl;
 					}
-					map = new Map(id, length, width);
-					cout << "Created new map with ID: " << id << endl;
-				}
-				else if (map_cmd == "2")
-				{
-					int id = -1;
-					SavedMapBuilder builder;
-
-					do
+					else if (map_cmd == "2")
 					{
-						cout << "Enter the map id of the map you want to edit: " << endl;
-						cin >> id;
-					} while (!builder.loadMap(id));
+						int id = -1;
+						SavedMapBuilder builder;
 
-					map = builder.getMap();
+						do
+						{
+							cout << "Enter the map id of the map you want to edit: " << endl;
+							cin >> id;
+						} while (!builder.loadMap(id));
 
-					
-				}
-				else if (map_cmd == "3")
-				{
-					mapedit = false;
-					break;
-				}
+						map = builder.getMap();
 
-				if (map != nullptr)
-				{
-					bool changeMap = true;
-					while (changeMap)
+
+					}
+					else if (map_cmd == "3")
 					{
-						cout << "What would you like to do with the map?" << endl;
-						cout << "1. Add Door" << endl;
-						cout << "2. Add Item" << endl;
-						cout << "3. Add Enemy" << endl;
-						cout << "4. Exit & Save" << endl;
-						cout << "5. Exit & Don't Save" << endl;
-						int editcmd;
-						cin >> editcmd;
+						mapedit = false;
+						break;
+					}
 
-						int x, y;
-						cout << "Enter x position: ";
-						cin >> x;
-						cout << "Enter y position: ";
-						cin >> y;
+					if (map != nullptr)
+					{
+						bool changeMap = true;
+						while (changeMap)
+						{
+							cout << "What would you like to do with the map?" << endl;
+							cout << "1. Add Door" << endl;
+							cout << "2. Add Item" << endl;
+							cout << "3. Add Enemy" << endl;
+							cout << "4. Exit & Save" << endl;
+							cout << "5. Exit & Don't Save" << endl;
+							int editcmd;
+							cin >> editcmd;
 
-						switch (editcmd)
-						{
-						case 1: //add door.
-						{
-							int mapid;
-							bool start;
-							cout << "Enter mapid that the door leads to: ";
-							cin >> mapid;
-							cout << "Is this a start or end position(1 for start, 0 for end): ";
-							cin >> start;
-							map->fillCell(x, y, new Door(mapid, start));
-							break;
-						}
-						case 2: // add item in this area
-						{
-							map->fillCell(x, y, new Item());
-							break;
-						}
-						case 3: //add enemy in this area
-						{
-							map->fillCell(x, y, new Enemy());
-							break;
-						}
-						case 4: // exit + save, no break function
-						{
-							map->saveMap();
-						}
-						case 5: //exit without saving.
-						{
-							changeMap = false;
-							break;
-						}
-						default:
-							cout << "Unknown Command" << endl;
-						}
+							int x, y;
+							cout << "Enter x position: ";
+							cin >> x;
+							cout << "Enter y position: ";
+							cin >> y;
 
-						
+							switch (editcmd)
+							{
+							case 1: //add door.
+							{
+								int mapid;
+								bool start;
+								cout << "Enter mapid that the door leads to: ";
+								cin >> mapid;
+								cout << "Is this a start or end position(1 for start, 0 for end): ";
+								cin >> start;
+								map->fillCell(x, y, new Door(mapid, start));
+								break;
+							}
+							case 2: // add item in this area
+							{
+								map->fillCell(x, y, new Item());
+								break;
+							}
+							case 3: //add enemy in this area
+							{
+								map->fillCell(x, y, new Enemy());
+								break;
+							}
+							case 4: // exit + save, no break function
+							{
+								map->saveMap();
+							}
+							case 5: //exit without saving.
+							{
+								changeMap = false;
+								break;
+							}
+							default:
+								cout << "Unknown Command" << endl;
+							}
+
+
+						}
 					}
 				}
+			} // Map FIN
+			case 3: // Create/Edit Character
+			{
+
+			} // Character FIN
+			
+			case 4: // Create/Edit Item
+			{
+
+			} // Item FIN
+
+			case 5: // Exit
+			{
+				run = false;
 			}
-		}
-		else if (cmd == "2") //exit.
-		{
-			run = false;
-		}
+		} // Switch Menu FIN
 	}
 }
 
