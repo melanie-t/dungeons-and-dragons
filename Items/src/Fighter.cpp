840b030f531862c531b4d6867f228e3fd1c8c527
Melanie Taing <melanie_taing@hotmail.com>
2016-11-11 00:47:53 -0500

    New: Constructor for loading fighters. loadFighter method

--- Items/src/Fighter.cpp
+++ Items/src/Fighter.cpp
@@ -3,6 +3,7 @@
 //!
 #include "Character.h"
 #include "Fighter.h"
+#include "Markup.h"
 #include <iostream> /* cout */
 
 using std::cout;
@@ -15,13 +16,26 @@ Fighter::Fighter(int level, int str, int dex, int con, int intel, int wis, int c
 	setCharClass(1);
 }
 
+//! Constructor: For loading Fighters
+Fighter::Fighter(int level, int str, int dex, int con, int intel, int wis, int cha, string name)
+{
+	setCharClass(1);
+	setLevel(level);
+	setSTR(str);
+	setDEX(dex);
+	setCON(con);
+	setINTEL(intel);
+	setWIS(wis);
+	setCHA(cha);
+	setName(name);
+}
+
 //! Default constructor: Assigns values for level 1
 Fighter::Fighter()
 {
 	setLevel(1);
 	setName("Unknown");
 	setCharClass(1);
-	statGenerator();
 }
 
 //! Constructor for Fighter
@@ -48,6 +62,59 @@ bool Fighter::validateNewFighter()
 	if (getDamageBonus() < 0)
 		return false;
 	return true;
+}
+
+Fighter* Fighter::loadFighter(string name)
+{
+	CMarkup xml;
+	if (xml.Load("characters/" + name + ".xml"))
+	{
+		xml.FindElem();
+		if (xml.GetTagName() != "character")
+		{
+			cout << "This file is not a character." << endl;
+			cout << xml.GetTagName() << endl;
+		}
+		else
+		{
+			xml.IntoElem();
+			int level, str, dex, con, intel, wis, cha;
+			while (xml.FindElem())
+			{
+				string tag = xml.GetTagName();
+				if (tag == "level")
+				{
+					level = atoi(xml.GetData().c_str());
+				}
+				else if (tag == "strength")
+				{
+					str = atoi(xml.GetData().c_str());
+				}
+				else if (tag == "dexterity")
+				{
+					dex = atoi(xml.GetData().c_str());
+				}
+				else if (tag == "constitution")
+				{
+					con = atoi(xml.GetData().c_str());
+				}
+				else if (tag == "intelligence")
+				{
+					intel = atoi(xml.GetData().c_str());
+				}
+				else if (tag == "wisdom")
+				{
+					wis = atoi(xml.GetData().c_str());
+				}
+				else if (tag == "charisma")
+				{
+					cha = atoi(xml.GetData().c_str());
+				}
+			}
+			return new Fighter(level, str, dex, con, intel, wis, cha, name);
+		}
+	}
+	return NULL; //Empty
 }
 
 
