e023953adb043ed50648ac8ece7befdd9cdaabad
Melanie Taing <melanie_taing@hotmail.com>
2016-11-10 21:46:20 -0500

    Changed everything in main menu to Switch statement

--- Items/src/Main.cpp
+++ Items/src/Main.cpp
@@ -46,112 +46,110 @@ void consoleInput()
 				cout << "1. Create Map" << endl;
 				cout << "2. Edit Map" << endl;
 				cout << "3. Exit Map Editor" << endl;
-				string map_cmd;
+				int map_cmd;
 				std::cin >> map_cmd;
 				Map* map;
-					if (map_cmd == "1") // create map
-					{
-						int width, length;
-						cout << "What length should the map be?" << endl;
-						cin >> length;
-						cout << "What width should the map be?" << endl;
-						cin >> width;
-						//map = new Map();
+				switch (map_cmd)
+				{
+				case 1: // Create Map
+				{
+					int width, length;
+					cout << "What length should the map be?" << endl;
+					cin >> length;
+					cout << "What width should the map be?" << endl;
+					cin >> width;
+					//map = new Map();
 
-						//find a better way of doing this.
-						int id = 1;
-						CMarkup xml;
-						char di[20];
+					//find a better way of doing this.
+					int id = 1;
+					CMarkup xml;
+					char di[20];
+					sprintf_s(di, 20, "maps/%d.xml", id);
+					while (xml.Load(string(di)))
+					{
+						id++;
 						sprintf_s(di, 20, "maps/%d.xml", id);
-						while (xml.Load(string(di)))
-						{
-							id++;
-							sprintf_s(di, 20, "maps/%d.xml", id);
+					}
+					map = new Map(id, length, width);
+					cout << "Created new map with ID: " << id << endl;
+				} // Create Map FIN
+				case 2: // Edit Map
+				{
+					int id = -1;
+					SavedMapBuilder builder;
+					do
+					{
+						cout << "Enter the map id of the map you want to edit: " << endl;
+						cin >> id;
+					} while (!builder.loadMap(id));
+					map = builder.getMap();
+				} // Edit Map FIN
+				case 3: // Exit Map
+				{
+					mapedit = false;
+					break;
+				} // Map Switch FIN
+				
+				if (map != nullptr)
+				{
+					bool changeMap = true;
+					while (changeMap)
+					{
+						cout << "What would you like to do with the map?" << endl;
+						cout << "1. Add Door" << endl;
+						cout << "2. Add Item" << endl;
+						cout << "3. Add Enemy" << endl;
+						cout << "4. Exit & Save" << endl;
+						cout << "5. Exit & Don't Save" << endl;
+					int editcmd;
+					cin >> editcmd;
+
+					int x, y;
+					cout << "Enter x position: ";
+					cin >> x;
+					cout << "Enter y position: ";
+					cin >> y;
+
+					switch (editcmd)
+					{
+					case 1: //add door.
+					{
+						int mapid;
+							bool start;
+							cout << "Enter mapid that the door leads to: ";
+							cin >> mapid;
+							cout << "Is this a start or end position(1 for start, 0 for end): ";
+							cin >> start;
+							map->fillCell(x, y, new Door(mapid, start));
+							break;
 						}
-						map = new Map(id, length, width);
-						cout << "Created new map with ID: " << id << endl;
-					}
-					else if (map_cmd == "2")
-					{
-						int id = -1;
-						SavedMapBuilder builder;
-
-						do
+						case 2: // add item in this area
 						{
-							cout << "Enter the map id of the map you want to edit: " << endl;
-							cin >> id;
-						} while (!builder.loadMap(id));
-
-						map = builder.getMap();
-
-
-					}
-					else if (map_cmd == "3")
-					{
-						mapedit = false;
-						break;
-					}
-
-					if (map != nullptr)
-					{
-						bool changeMap = true;
-						while (changeMap)
-						{
-							cout << "What would you like to do with the map?" << endl;
-							cout << "1. Add Door" << endl;
-							cout << "2. Add Item" << endl;
-							cout << "3. Add Enemy" << endl;
-							cout << "4. Exit & Save" << endl;
-							cout << "5. Exit & Don't Save" << endl;
-							int editcmd;
-							cin >> editcmd;
-
-							int x, y;
-							cout << "Enter x position: ";
-							cin >> x;
-							cout << "Enter y position: ";
-							cin >> y;
-
-							switch (editcmd)
-							{
-							case 1: //add door.
-							{
-								int mapid;
-								bool start;
-								cout << "Enter mapid that the door leads to: ";
-								cin >> mapid;
-								cout << "Is this a start or end position(1 for start, 0 for end): ";
-								cin >> start;
-								map->fillCell(x, y, new Door(mapid, start));
-								break;
-							}
-							case 2: // add item in this area
-							{
-								map->fillCell(x, y, new Item());
-								break;
-							}
-							case 3: //add enemy in this area
-							{
-								map->fillCell(x, y, new Enemy());
-								break;
-							}
-							case 4: // exit + save, no break function
-							{
-								map->saveMap();
-							}
-							case 5: //exit without saving.
-							{
-								changeMap = false;
-								break;
-							}
-							default:
-								cout << "Unknown Command" << endl;
-							}
-
-
+							map->fillCell(x, y, new Item());
+							break;
 						}
+						case 3: //add enemy in this area
+						{
+							map->fillCell(x, y, new Enemy());
+							break;
+						}
+						case 4: // exit + save, no break function
+						{
+							map->saveMap();
+						}
+						case 5: //exit without saving.
+						{
+							changeMap = false;
+							break;
+						}
+						default:
+							cout << "Unknown Command" << endl;
+						}
+
+
 					}
 				}
+			}
 			} // Map FIN
 			case 3: // Create/Edit Character
 			{
